f9cdfcb54e6f41e4b864232f786f32060048aac3
ImanuelAtaama <imanuelataama11@gmail.com>
2025-03-07 22:07:09 +0700

    Update fitur upload ke Cloudinary

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-# app.py
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import cloudinary
+import cloudinary.uploader
 from werkzeug.utils import secure_filename
 from model import load_model, predict_image
 
 app = Flask(__name__)
 
-# Folder untuk menyimpan file yang diunggah
-UPLOAD_FOLDER = 'static/uploads/'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# Konfigurasi Cloudinary (Ganti dengan kredensial akun Cloudinary Anda)
+cloudinary.config(
+    cloud_name= 'dqj4dyfne', 
+    api_key= '674711576519494', 
+    api_secret= '6YIquAcxY0fIvcosGW04y2QZG3o'
+)
 
 # Load model DenseNet121
 model = load_model()
@@ -31,16 +33,19 @@ def image_inference():
             filename = secure_filename(file.filename)
             if not filename.lower().endswith('.jpg'):
                 return render_template('image.html', error="File harus memiliki ekstensi .jpg")
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            return redirect(url_for('result', filename=filename))
+            
+            # Upload ke Cloudinary
+            result = cloudinary.uploader.upload(file)
+            file_url = result["secure_url"]  # URL gambar setelah diunggah ke Cloudinary
+            
+            return redirect(url_for('result', file_url=file_url))
     return render_template('image.html')
 
-@app.route('/result/<filename>')
-def result(filename):
-    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    label= predict_image(model, filepath)
-    return render_template('result.html', filename=filename, label=label)
+@app.route('/result')
+def result():
+    file_url = request.args.get('file_url')
+    label = predict_image(model, file_url)  # Kirim URL ke model untuk prediksi
+    return render_template('result.html', file_url=file_url, label=label)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=False)
